29b2124f5d22f153c0366470e6251acf12a687b1
junedali1995 <junedali.sk@gmail.com>
2023-10-04 09:15:48 +0530

    updated mongodb

--- frontend.sh
+++ frontend.sh
@@ -8,23 +8,23 @@
 #echo -e "\e[36m This is message prints in Cyan \e[0m"
 
 echo -e "\e[36m Install Nginx \e[0m"
-yum install nginx -y
+yum install nginx -y &>>/tmp/roboshop.log
 
 echo -e "\e[36m Start & Enable Nginx service \e[0m"
-systemctl enable nginx
-systemctl start nginx
+systemctl enable nginx &>>/tmp/roboshop.log
+systemctl start nginx &>>/tmp/roboshop.log
 
 echo -e "\e[36m Remove the default content that web server is serving \e[0m"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>>/tmp/roboshop.log
 
 echo -e "\e[36m Download the frontend content \e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/roboshop.log
 
 echo -e "\e[36m Extract the frontend content \e[0m"
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+cd /usr/share/nginx/html &>>/tmp/roboshop.log
+unzip /tmp/frontend.zip &>>/tmp/roboshop.log
 
 #Copy the roboshop.conf file
 
 echo -e "\e[36m Restart Nginx Service to load the changes of the configuration \e[0m"
-systemctl restart nginx
+systemctl restart nginx &>>/tmp/roboshop.log
--- mongodb.sh
+++ mongodb.sh
@@ -1,6 +1,27 @@
-echo -e "\e[31m Hello in Red colour \e[0m"
-echo -e "\e[32m Hello in Green colour \e[0m"
-echo -e "\e[33m Hello in Yellow colour \e[0m"
-echo -e "\e[34m Hello in Blue colour \e[0m"
-echo -e "\e[35m Hello in Magenta colour \e[0m"
-echo -e "\e[36m Hello in Cyan colour \e[0m"
+#echo -e "\e[31m Hello in Red colour \e[0m"
+#echo -e "\e[32m Hello in Green colour \e[0m"
+#echo -e "\e[33m Hello in Yellow colour \e[0m"
+#echo -e "\e[34m Hello in Blue colour \e[0m"
+#echo -e "\e[35m Hello in Magenta colour \e[0m"
+#echo -e "\e[36m Hello in Cyan colour \e[0m"
+
+echo -e "\e[35m Copy mongo.repo file \e[0m"
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
+
+echo -e "\e[35m Install MongoDB \e[0m"
+dnf install mongodb-org -y &>>/tmp/roboshop.log
+
+#update listen address from 127.0.0.1 to 0.0.0.0
+
+echo -e "\e[35m Start & Enable MongoDB Service \e[0m"
+systemctl enable mongod &>>/tmp/roboshop.log
+systemctl start mongod &>>/tmp/roboshop.log
+
+
+
+
+
+
+
+
+
