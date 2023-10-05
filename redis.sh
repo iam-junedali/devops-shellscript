a6eb442f6c92b67f09386997816b271a01aa6a41
junedali1995 <junedali.sk@gmail.com>
2023-10-05 18:26:09 +0530

    redis

---
+++ redis.sh
@@ -0,0 +1,17 @@
+echo -e "\e[36m Install Redis Repos \e[0m"
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+
+echo -e "\e[36m Enable Redis 6 version \e[0m"
+dnf module enable redis:remi-6.2 -y
+
+echo -e "\e[36m Install Redis \e[0m"
+dnf install redis -y
+
+echo -e "\e[36m Install Redis Repos \e[0m"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf
+
+echo -e "\e[36m Enable Redis\e[0m"
+systemctl enable redis
+
+echo -e "\e[36m Start Redis \e[0m"
+systemctl restart redis
