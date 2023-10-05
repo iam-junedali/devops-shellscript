fe9951178db75fcd3e1c33d9c54beb6654af7e62
junedali1995 <junedali.sk@gmail.com>
2023-10-05 18:27:40 +0530

    redis

--- redis.sh
+++ redis.sh
@@ -1,17 +1,17 @@
 echo -e "\e[36m Install Redis Repos \e[0m"
-dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>/tmp/roboshop.log
 
 echo -e "\e[36m Enable Redis 6 version \e[0m"
-dnf module enable redis:remi-6.2 -y
+dnf module enable redis:remi-6.2 -y &>>/tmp/roboshop.log
 
 echo -e "\e[36m Install Redis \e[0m"
-dnf install redis -y
+dnf install redis -y &>>/tmp/roboshop.log
 
 echo -e "\e[36m Install Redis Repos \e[0m"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>/tmp/roboshop.log
 
 echo -e "\e[36m Enable Redis\e[0m"
-systemctl enable redis
+systemctl enable redis &>>/tmp/roboshop.log
 
 echo -e "\e[36m Start Redis \e[0m"
-systemctl restart redis
+systemctl restart redis &>>/tmp/roboshop.log
