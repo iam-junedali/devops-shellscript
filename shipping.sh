3eb4fc22287745922efeacd01eec054c1c3c4d2b
junedali1995 <junedali.sk@gmail.com>
2023-10-05 19:33:13 +0530

    updated log file in shipping

--- shipping.sh
+++ shipping.sh
@@ -1,43 +1,43 @@
 echo -e "\e[36m Install Maven \e[0m"
-dnf install maven -y
+dnf install maven -y &>>/tmp/roboshop.log
 
 echo -e "\e[36m Add user Roboshop \e[0m"
-useradd roboshop
+useradd roboshop &>>/tmp/roboshop.log
 
 echo -e "\e[36m create app \e[0m"
 rm -rf /app &>>/tmp/roboshop.log
-mkdir /app
+mkdir /app &>>/tmp/roboshop.log
 
 echo -e "\e[36m Install code \e[0m"
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
+curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip &>>/tmp/roboshop.log
 
 echo -e "\e[36m change to app directory \e[0m"
-cd /app
+cd /app &>>/tmp/roboshop.log
 
 echo -e "\e[36m Unzip the code \e[0m"
-unzip /tmp/shipping.zip
+unzip /tmp/shipping.zip &>>/tmp/roboshop.log
 
 echo -e "\e[36m Maven clean \e[0m"
-mvn clean package
+mvn clean package &>>/tmp/roboshop.log
 
 echo -e "\e[36m Install Jar file \e[0m"
-mv target/shipping-1.0.jar shipping.jar
+mv target/shipping-1.0.jar shipping.jar &>>/tmp/roboshop.log
 
 echo -e "\e[36m Copy Systemd Shipping file \e[0m"
-cp /home/centos/devops-shellscript/shipping.service /etc/systemd/system/shipping.service
+cp /home/centos/devops-shellscript/shipping.service /etc/systemd/system/shipping.service &>>/tmp/roboshop.log
 
 echo -e "\e[36m daemon reload \e[0m"
-systemctl daemon-reload
+systemctl daemon-reload &>>/tmp/roboshop.log
 
 echo -e "\e[36m Enable and start shipping \e[0m"
-systemctl enable shipping
-systemctl start shipping
+systemctl enable shipping &>>/tmp/roboshop.log
+systemctl start shipping &>>/tmp/roboshop.log
 
 echo -e "\e[36m Install MySQL \e[0m"
-dnf install mysql -y
+dnf install mysql -y &>>/tmp/roboshop.log
 
 echo -e "\e[36m Install Schema \e[0m"
-mysql -h mysql-dev.joyousgroups.com -uroot -pRoboShop@1 < /app/schema/shipping.sql
+mysql -h mysql-dev.joyousgroups.com -uroot -pRoboShop@1 < /app/schema/shipping.sql &>>/tmp/roboshop.log
 
 echo -e "\e[36m Restart Shipping \e[0m"
-systemctl restart shipping
+systemctl restart shipping &>>/tmp/roboshop.log
