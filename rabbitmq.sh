69e52c9084a6b5792e94c54a7eb9d6b8867a693d
junedali1995 <junedali.sk@gmail.com>
2023-10-06 12:07:53 +0530

    created rabbitmq

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,18 +1,18 @@
 echo -e "\e[36m Download Erlang script \e[0m"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>/tmp/roboshop.log
 
 echo -e "\e[36m Configure Yum repos \e[0m"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>/tmp/roboshop.log
 
 echo -e "\e[36m Install RabbitMQ \e[0m"
-dnf install rabbitmq-server -y
+dnf install rabbitmq-server -y &>>/tmp/roboshop.log
 
 echo -e "\e[36m Enable and start rabbitmq-server \e[0m"
-systemctl enable rabbitmq-server
-systemctl start rabbitmq-server
+systemctl enable rabbitmq-server &>>/tmp/roboshop.log
+systemctl start rabbitmq-server &>>/tmp/roboshop.log
 
 echo -e "\e[36m Add user \e[0m"
-rabbitmqctl add_user roboshop roboshop123
+rabbitmqctl add_user roboshop roboshop123 &>>/tmp/roboshop.log
 
 echo -e "\e[36m Set permissions \e[0m"
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>/tmp/roboshop.log
