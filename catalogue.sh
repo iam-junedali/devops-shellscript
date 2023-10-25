228bd04a87a23d822c71673acbfe47135e9307a6
junedali1995 <junedali.sk@gmail.com>
2023-10-25 21:01:36 +0530

    commit

--- catalogue.sh
+++ catalogue.sh
@@ -1,3 +1,5 @@
+component=catalogue
+
 echo -e "\e[36m Configuring NodeJS repos \e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
 
@@ -12,13 +14,13 @@ rm -rf /app &>>/tmp/roboshop.log
 mkdir /app &>>/tmp/roboshop.log
 
 echo -e "\e[36m Download Application content \e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop.log
+curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>/tmp/roboshop.log
 
 echo -e "\e[36m Change to app directory \e[0m"
 cd /app
 
 echo -e "\e[36m Extract Application content \e[0m"
-unzip /tmp/catalogue.zip &>>/tmp/roboshop.log
+unzip /tmp/$component.zip &>>/tmp/roboshop.log
 
 echo -e "\e[36m change to app directory \e[0m"
 cd /app
@@ -27,14 +29,14 @@ echo -e "\e[36m Install NodeJS Dependencies \e[0m"
 npm install &>>/tmp/roboshop.log
 
 echo -e "\e[36m setup systemd service \e[0m"
-cp /home/centos/devops-shellscript/catalogue.service /etc/systemd/system/catalogue.service
+cp /home/centos/devops-shellscript/$component.service /etc/systemd/system/$component.service
 
 echo -e "\e[36m Daemon reload \e[0m"
 systemctl daemon-reload
 
 echo -e "\e[36m Enable and restart catalogue service \e[0m"
-systemctl enable catalogue &>>/tmp/roboshop.log
-systemctl restart catalogue &>>/tmp/roboshop.log
+systemctl enable $component &>>/tmp/roboshop.log
+systemctl restart $component &>>/tmp/roboshop.log
 
 echo -e "\e[36m Copy mongo.repo file \e[0m"
 cp /home/centos/devops-shellscript/mongo.repo /etc/yum.repos.d/mongo.repo
@@ -43,4 +45,4 @@ echo -e "\e[36m Install mongo-shell \e[0m"
 dnf install mongodb-org-shell -y &>>/tmp/roboshop.log
 
 echo -e "\e[36m Configure Host \e[0m"
-mongo --host mongodb-dev.joyousgroups.com </app/schema/catalogue.js &>>/tmp/roboshop.log
+mongo --host mongodb-dev.joyousgroups.com </app/schema/$component.js &>>/tmp/roboshop.log
