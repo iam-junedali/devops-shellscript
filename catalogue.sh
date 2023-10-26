8e2fcb205dab999afe1320d847730dbff4689616
junedali1995 <junedali.sk@gmail.com>
2023-10-26 11:08:48 +0530

    catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -1,48 +1,50 @@
 component=catalogue
+colour="\e[36m"
+nocolour="\e[0m"
 
-echo -e "\e[36m Configuring NodeJS repos \e[0m"
+echo -e "${colour} Configuring NodeJS repos ${nocolour}"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
 
-echo -e "\e[36m Install NodeJS \e[0m"
+echo -e "${colour} Install NodeJS ${nocolour}"
 dnf install nodejs -y &>>/tmp/roboshop.log
 
-echo -e "\e[36m Add Roboshop User \e[0m"
+echo -e "${colour} Add Roboshop User ${nocolour}"
 useradd roboshop &>>/tmp/roboshop.log
 
-echo -e "\e[36m Create application directory \e[0m"
+echo -e "${colour} Create application directory ${nocolour}"
 rm -rf /app &>>/tmp/roboshop.log
 mkdir /app &>>/tmp/roboshop.log
 
-echo -e "\e[36m Download Application content \e[0m"
+echo -e "${colour} Download Application content ${nocolour}"
 curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>/tmp/roboshop.log
 
-echo -e "\e[36m Change to app directory \e[0m"
+echo -e "${colour} Change to app directory ${nocolour}"
 cd /app
 
-echo -e "\e[36m Extract Application content \e[0m"
+echo -e "${colour} Extract Application content ${nocolour}"
 unzip /tmp/$component.zip &>>/tmp/roboshop.log
 
-echo -e "\e[36m change to app directory \e[0m"
+echo -e "${colour} change to app directory ${nocolour}"
 cd /app
 
-echo -e "\e[36m Install NodeJS Dependencies \e[0m"
+echo -e "${colour} Install NodeJS Dependencies ${nocolour}"
 npm install &>>/tmp/roboshop.log
 
-echo -e "\e[36m setup systemd service \e[0m"
+echo -e "${colour} setup systemd service ${nocolour}"
 cp /home/centos/devops-shellscript/$component.service /etc/systemd/system/$component.service
 
-echo -e "\e[36m Daemon reload \e[0m"
+echo -e "${colour} Daemon reload ${nocolour}"
 systemctl daemon-reload
 
-echo -e "\e[36m Enable and restart catalogue service \e[0m"
+echo -e "${colour} Enable and restart catalogue service ${nocolour}"
 systemctl enable $component &>>/tmp/roboshop.log
 systemctl restart $component &>>/tmp/roboshop.log
 
-echo -e "\e[36m Copy mongo.repo file \e[0m"
+echo -e "${colour} Copy mongo.repo file ${nocolour}"
 cp /home/centos/devops-shellscript/mongo.repo /etc/yum.repos.d/mongo.repo
 
-echo -e "\e[36m Install mongo-shell \e[0m"
+echo -e "${colour} Install mongo-shell ${nocolour}"
 dnf install mongodb-org-shell -y &>>/tmp/roboshop.log
 
-echo -e "\e[36m Configure Host \e[0m"
+echo -e "${colour} Configure Host ${nocolour}"
 mongo --host mongodb-dev.joyousgroups.com </app/schema/$component.js &>>/tmp/roboshop.log
